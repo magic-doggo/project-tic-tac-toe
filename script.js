33abfd7f70eba6222f43c1291c49de7d48495fbd
magic-doggo <109861946+magic-doggo@users.noreply.github.com>
2024-03-05 19:43:30 +0200

    Add css styling

--- script.js
+++ script.js
@@ -12,21 +12,27 @@
 //         sign: 2
 //     }
 // ]
-
+// let container = document.getElementById("container")
 
 function gameBoard() {
-    return {cells: [null, null, "asd", null, null, null, null, null, null]}
+    return {cells: ["X", "O", "asd", 4, null, null, null, null, null]}
 }
 
 gameObject = gameBoard()
 console.log(gameObject.cells)
 
-gameObject.cells.forEach(pasta => {
-    let newCell = document.createElement("div")
-    document.body.appendChild(newCell)
-    newCell.textContent = "asdasd"
-    newCell.addEventListener("click", placeToken)
-    function placeToken(){
+function updateBoard() {
+    gameObject.cells.forEach(pasta => {
+        let newCell = document.createElement("div")
+        document.getElementById("container").appendChild(newCell)
+        newCell.textContent = pasta
+
+        newCell.addEventListener("click", placeToken) //maybe put this somewhere else?
+        function placeToken(){
+            console.log(pasta)
+        } //could i run the updateBoard() function here, to run every time a cell is clicked(token placed), only of cell is not x/o
         console.log(pasta)
-    }
-})
+    })
+}
+
+updateBoard()
