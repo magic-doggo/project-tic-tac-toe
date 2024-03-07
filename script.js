d8d308141ac65ec0beb73ecfa8586f7de56685bb
magic-doggo <109861946+magic-doggo@users.noreply.github.com>
2024-03-07 14:05:24 +0200

    Alternate player turns to place token on display and modify array

--- script.js
+++ script.js
@@ -1,45 +1,50 @@
-// const players = [
-//     {
-//         name: playerOneName,
-//         sign: "X"
-//     },
-//     {
-//         name: playerTwoName,
-//         sign: "O"
-//     }
-// ]
+const players = [
+    {
+        name: "alex",
+        sign: "X"
+    },
+    {
+        name: "not alex",
+        sign: "O"
+    }
+]
 
-// let activePlayer = players[0]
-// const switchPlayerTurn = () => {
-//     if (activePlayer = players[0]) {
-//         activePlayer = players[1]
-//     }
-//     else {
-//         activePlayer = players[0]
-//     }
-// }
+let activePlayer = players[0]
+const switchPlayerTurn = function() {
+    if (activePlayer == players[0]) {
+        activePlayer = players[1];
+    }
+    else {
+        activePlayer = players[0];
+    }
+}
+// const getActivePlayer = () => activePlayer;
 
-// let container = document.getElementById("container")
 
 function gameBoard() {
-    return {cells: ["X", "O", "asd", 4, null, null, null, null, null]}
+    return {cells: [null, null, null, null, null, null, null, null, null]}
 }
 
-const gameObject = gameBoard() //works without let? should add it
+const gameObject = gameBoard()
 console.log(gameObject.cells)
 
-function createBoard() { //or maybe do without this function?
-    gameObject.cells.forEach(pasta => {
+function createBoard() {
+    gameObject.cells.forEach((pasta, index, cellsArray) => {
         let newCell = document.createElement("div")
         document.getElementById("container").appendChild(newCell)
+        
         newCell.addEventListener("click", placeToken)
-
         function placeToken(){
-            console.log(pasta)
-            newCell.textContent = pasta //wont be needed when match starts? or will it?
+            if (cellsArray[index] == null) {
+                cellsArray[index] = activePlayer.sign;
+                newCell.textContent = activePlayer.sign;
+                console.log(gameObject.cells);
+                switchPlayerTurn();
+            }
+            else return;
         }
-        // } //could i run the updateBoard() function here, to run every time a cell is clicked(token placed), only of cell is not x/o
     })
 }
 
 createBoard()
+console.log(activePlayer.name)
